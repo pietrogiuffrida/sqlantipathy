389513f49be280f7efeab4d688ec83f784ccc71e
Pietro Giuffrida <p.giuffrida@crif.com>
2019-07-13 14:46:35 +0200

    Setupu.py initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sqlantipathy',
+      version='0.1',
+      description='Python library to easily work with SQL DB',
+      url='https://github.com/pietrogiuffrida/sqlantipathy/',
+      author='Pietro Giuffrida',
+      author_email='pietro.giuffri@gmail.com',
+      license='GPL V3.0',
+      packages=['sqlantipathy'],
+      zip_safe=False)
