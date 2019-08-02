a4580d2b1a5c3227e27b7510c6f7910c4de545d0
Pietro Giuffrida <p.giuffrida@crif.com>
2019-08-02 19:16:05 +0200

    Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name="sqlantipathy",
-      version="0.0.5    ",
+      version="0.0.6",
       url="https://github.com/pietrogiuffrida/sqlantipathy/",
       author="Pietro Giuffrida",
       author_email="pietro.giuffri@gmail.com",
